4031de9cde6751d37eec8006a5cb590b0d4d111b
yvonnegichovi <yvonnegichovi@gmail.com>
2024-10-08 20:10:47 +0300

    database migration

--- entry/__init__.py
+++ entry/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from dotenv import load_dotenv
+from flask_migrate import Migrate
 
 load_dotenv()
 
@@ -16,6 +17,8 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'info'
+
+migrate = Migrate(app, db)
 
 from entry.routes.main_routes import main
 from entry.routes.user_routes import auth
